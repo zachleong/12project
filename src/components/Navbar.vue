93af27c0d2741c18ade2a739416dc3c204b25c28
Zach Leong <earthospreyhd@gmail.com>
2019-05-21 19:36:54 +1000

    Made navbar sticky

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,31 +1,29 @@
 <template>
-  <div class="center">
-    <div class="navbar">
-      <a class="brand navlink">
-        <h2>Git Money</h2>
-      </a>
-      <router-link to="/" class="navlink">Home</router-link>
-      <router-link to="/projects" class="navlink">Projects</router-link>
-      <router-link to="/createproject" class="navlink"
-        >Create Project</router-link
+  <div class="navbar">
+    <a class="brand navlink">
+      <h2>Git Money</h2>
+    </a>
+    <router-link to="/" class="navlink">Home</router-link>
+    <router-link to="/projects" class="navlink">Projects</router-link>
+    <router-link to="/createproject" class="navlink"
+      >Create Project</router-link
+    >
+    <div id="authButton">
+      <el-button
+        v-if="userIsAuth === true"
+        v-on:click="signOut"
+        type="primary"
+        plain
+        >Sign out</el-button
       >
-      <div id="authButton">
-        <el-button
-          v-if="userIsAuth === true"
-          v-on:click="signOut"
-          type="primary"
-          plain
-          >Sign out</el-button
-        >
-        <button
-          v-else-if="userIsAuth === false"
-          v-on:click="goToLogin"
-          type="primary"
-          class="button login-button"
-        >
-          Login
-        </button>
-      </div>
+      <button
+        v-else-if="userIsAuth === false"
+        v-on:click="goToLogin"
+        type="primary"
+        class="button login-button"
+      >
+        Login
+      </button>
     </div>
   </div>
 </template>
@@ -94,9 +92,10 @@ export default {
   padding: 0 15px 0 15px;
 }
 .navbar {
-  position: relative;
+  position: sticky;
+  top: 0;
   width: 100%;
-  height: 70px;
+  //   height: 70px;
   display: inline-block;
   -webkit-box-shadow: 0 1px 7px 0 rgba(0, 0, 0, 0.1);
   box-shadow: 0 1px 7px 0 rgba(0, 0, 0, 0.1);
