237c45501240d84bfd3983c0ee2249508f40434b
Zach Leong <earthospreyhd@gmail.com>
2019-06-19 17:56:20 +1000

    Added coding languages to projects

--- src/views/CreateProject.vue
+++ src/views/CreateProject.vue
@@ -7,6 +7,21 @@
       placeholder="Title"
       class="input project-in"
     />
+    <el-select
+      v-model="project.languages"
+      multiple
+      placeholder="Coding languages"
+      class="lang-select"
+    >
+      <el-option
+        v-for="item in options"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value"
+        class="lang-options"
+      >
+      </el-option>
+    </el-select>
     <textarea
       type="text"
       v-model="project.projectDesc"
@@ -28,9 +43,24 @@ export default {
     return {
       project: {
         projectTitle: "",
-        projectDesc: ""
+        projectDesc: "",
+        languages: []
       },
-      projectIsSet: false
+      projectIsSet: false,
+      options: [
+        {
+          value: "Javascript",
+          label: "Javascript"
+        },
+        {
+          value: "Python",
+          label: "Python"
+        },
+        {
+          value: "C/C++",
+          label: "C/C++"
+        }
+      ]
     };
   },
   methods: {
@@ -47,15 +77,28 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .project-in {
   margin: 10px 0;
-  width: 65%;
+  width: 49%;
 }
 .project-desc {
   transition: border 0.4s;
   width: 65%;
   height: 200px;
   resize: vertical;
+}
+.lang-select {
+  margin-left: 1%;
+  font-family: "Nunito", sans-serif;
+}
+.lang-options {
+  font-family: "Nunito", sans-serif;
+}
+.el-input__inner {
+  font-size: 16px !important;
+  font-family: "Nunito", sans-serif;
+  height: 44px !important;
+  border: 1px solid #ebeef5 !important;
 }
 </style>
